a447a7b37471447a7c6246e1089ccd55367f0741
Your Name <you@example.com>
2021-08-27 09:08:29 +0000

    テンプレートテスト

--- src/spec/requests/static_pages_spec.rb
+++ src/spec/requests/static_pages_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
+    #HTTP通信のテスト
     it "/homeルーティングで画面が正常に表示される" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
     end
+  
   end
 
   describe "GET /help" do
