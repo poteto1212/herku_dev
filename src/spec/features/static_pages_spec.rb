36df9fcde2a7f91ff247275c96e646b8772f6ec3
Your Name <you@example.com>
2021-08-29 12:47:14 +0000

    共通テンプレートテストクリア

--- src/spec/features/static_pages_spec.rb
+++ src/spec/features/static_pages_spec.rb
@@ -1,25 +1,33 @@
 require 'rails_helper'
 #テンプレートに含まれる文字をテストしたいときに書く
-describe 'Home' do
-  specify '画面の表示' do
-    visit '/static_pages/home'
-    expect(page).to have_css('h1', text: 'Sample App')
-    expect(page).to have_css('title',text: "Home | Ruby on Rails Tutorial Sample App")
-  end
-end
+RSpec.feature "static_pages",type: :feature do
 
-describe 'Help' do
-  specify '画面の表示' do
-    visit '/static_pages/help'
-    expect(page).to have_css('h1', text: 'Help')
-    expect(page).to  have_css('title',text: "Help | Ruby on Rails Tutorial Sample App")
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
-end
 
-describe 'About' do
-  specify '画面の表示' do
-    visit '/static_pages/about'
-    expect(page).to have_css('h1', text: 'About')
-    expect(page).to  have_css('title',text: "About | Ruby on Rails Tutorial Sample App")
+  describe 'Home' do
+    specify '画面の表示' do
+      
+      visit '/static_pages/home'
+      expect(page).to have_css('h1', text: 'Sample App')
+      expect(page).to have_title "Home | #{@base_title}"
+    end
+  end
+
+  describe 'Help' do
+    specify '画面の表示' do
+      visit '/static_pages/help'
+      expect(page).to have_css('h1', text: 'Help')
+      expect(page).to  have_title "Help | #{@base_title}"
+    end
+  end
+
+  describe 'About' do
+    specify '画面の表示' do
+      visit '/static_pages/about'
+      expect(page).to have_css('h1', text: 'About')
+      expect(page).to  have_title "About | #{@base_title}"
+    end
   end
 end
